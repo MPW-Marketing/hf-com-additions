fb2a89321c13681881f6f1bd9ba687ecd1060248
damonmaldonado <damon@mpwmarketing.com>
2016-03-22 11:41:39 -0400

    added selector for current value

--- js/holland-commerce.js
+++ js/holland-commerce.js
@@ -5,3 +5,10 @@ sumjq = function(selector) {
     });
     return sum;
 }
+
+currentCookies = function(selector) {
+    var current = 0;
+    var selectValue = jQuery( selector ).val();
+    current = selectValue.substring(0,2);
+    return current;
+}
