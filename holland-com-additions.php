863976807a4a959d8f5548f1d1ef831857f73026
Damon Maldonado <damon@mpwmarketing.com>
2016-03-23 10:07:12 -0400

    additions

--- holland-com-additions.php
+++ holland-com-additions.php
@@ -10,3 +10,11 @@ function hf_com_scripts_method() {
     wp_enqueue_script( 'holland-commerce', plugins_url( '/js/holland-commerce.js' , __FILE__ ), array( 'jquery' ), '1.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'hf_com_scripts_method' );
+
+
+function add_total_cookies_fields () {
+$cont = '<div class="cookies-selected">You have selected <span id="selected-cookies-number">0</span></div>';
+echo $cont;
+}
+
+//add_action( 'woocommerce_single_product_summary', 'add_total_cookies_fields', 100, 0 );
--- js/holland-commerce.js
+++ js/holland-commerce.js
@@ -12,3 +12,73 @@ currentCookies = function(selector) {
     current = selectValue.substring(0,2);
     return current;
 }
+
+orderSizeChange = function(selector, summary){
+	var totCookies = currentCookies(selector);
+	var selectCookies = sumjq(summary);
+	if(jQuery.isNumeric(totCookies)){
+		var remainCookies = totCookies - selectCookies;
+		jQuery('.cookies-selected').html('You have selected <span id="cookies-selected-number">'+selectCookies+'</span> of <span id="cookies-total-number">'+totCook+'</span> cookies. Please select <span id="cookies-left">'+remainCookies+'</span> more cookies.');
+	}
+
+}
+
+cookiesNumberChange = function (selector, summary){
+	var totCookies = currentCookies(selector);
+	var selectCookies = sumjq(summary);
+  	if(jQuery.isNumeric(totCookies)){
+  		var remainCookies = totCookies - selectCookies;
+  		jQuery('#cookies-selected-number').html(selectCookies);
+		jQuery('#cookies-total-number').html(totCookies);
+		jQuery('#cookies-left').html(remainCookies);
+  	}
+}
+jQuery( document ).ready( function () {
+		jQuery( '.variations' ).on('change', '#pa_order-size' , function() {
+		console.log('selec');
+  		var selectCookies = sumjq('.tm-qty');
+  		var totCookies = currentCookies('#pa_order-size');
+			if(jQuery.isNumeric(totCookies)){
+				var remainCookies = totCookies - selectCookies;
+				jQuery('.cookies-selected').html('You have selected <span id="cookies-selected-number" class="cookie-number">'+selectCookies+'</span> of <span id="cookies-total-number" class="cookie-number">'+totCookies+'</span> cookies. <span class="cookie-alert"></span>');
+				if (remainCookies > 0 ) {
+				jQuery('.cookie-alert').html('Please select <span id="cookies-left" class="cookie-number">'+remainCookies+'</span> more cookies.');
+				} else if (remainCookies < 0) {
+					jQuery('.cookie-alert').html('You have selected <span id="cookies-left" class="cookie-number">'+Math.abs(remainCookies)+'</span> too many cookies, please adjust your order size or number of cookies');				} else if (remainCookies == 0 ) {
+				jQuery('.cookie-alert').html('');
+				} 
+			}
+	});
+
+
+	jQuery( '.tm-qty' ).change(function() {
+		var thisVal = jQuery(this).val()
+			var holder = jQuery(this).parent().next('.tm-field-display').children('.tmcp-checkbox');
+			var thyname = jQuery(this).prop('name');
+			thyname = thyname.replace('_quantity' , '');
+			holder = jQuery('.tmcp-checkbox[name="'+thyname+'"]');
+		if (thisVal > 0) {
+			jQuery(holder).prop( "checked", true );
+		} else if (thisVal == 0) {
+			jQuery(holder).prop( "checked", false );
+		}
+
+  		var selectCookies = sumjq('.tm-qty');
+  		var totCookies = currentCookies('#pa_order-size');
+  		if(jQuery.isNumeric(totCookies)){
+  			var remainCookies = totCookies - selectCookies;
+  			jQuery('#cookies-selected-number').html(selectCookies);
+			jQuery('#cookies-total-number').html(totCookies);
+			jQuery('#cookies-left').html(remainCookies);
+			if (remainCookies > 0 ) {
+				jQuery('.cookie-alert').html('Please select <span id="cookies-left" class="cookie-number">'+remainCookies+'</span> more cookies.');
+				} else if (remainCookies < 0) {
+					jQuery('.cookie-alert').html('You have selected <span id="cookies-left" class="cookie-number">'+Math.abs(remainCookies)+'</span> too many cookies, please adjust your order size or number of cookies');
+				} else if (remainCookies == 0 ) {
+				jQuery('.cookie-alert').html('');
+				} 
+  		}
+  		console.log(totCookies+'cha');
+	});
+	
+});
